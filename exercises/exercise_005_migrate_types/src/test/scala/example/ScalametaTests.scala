59b2dfd0519af431feaad1fc82a368f39ac266e8
Adrien Piquerez <adrien.piquerez@gmail.com>
2023-09-04 15:51:05 +0200

    Minor changes

--- exercises/exercise_005_migrate_types/src/test/scala/example/ScalametaTests.scala
+++ exercises/exercise_005_migrate_types/src/test/scala/example/ScalametaTests.scala
@@ -6,7 +6,7 @@ class ScalametaTests extends munit.FunSuite {
   test("parse Scala expression") {
     val expression = Scalameta.parseExpression("xs.map(_.size)")
     expression match {
-      case Term.Apply(Term.Select(Term.Name("xs"), Term.Name("map")), _) => ()
+      case Term.Apply.After_4_6_0(Term.Select(Term.Name("xs"), Term.Name("map")), _) => ()
       case _ => fail("wrong expression", clues(expression))
     }
   }
